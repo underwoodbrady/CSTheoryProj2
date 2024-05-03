279aa601c69c376f7df0f5a551d903e9c1a6471e
emeryKrulish <164568907+emeryKrulish@users.noreply.github.com>
2024-05-03 15:10:26 -0600

    Fixed Union
    Basically just broke it into a bunch of helper methods, turned out the transitions were fucked up as hell. You should be able to do the same for concatenate, take a stab at it and if you can't get it I'll deal with it on Sunday.

--- RegularExpression.java
+++ RegularExpression.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 /**
  *
  * @author yaw
@@ -22,116 +23,196 @@ public class RegularExpression {
 
     // TODO: Complete this method so that it returns the nfa resulting from unioning the two input nfas.
     private NFA union(NFA nfa1, NFA nfa2) {
-
-        //combine the states by putting both into a set
+        // Create a unique set of states and transitions
         HashSet<String> newStates = new HashSet<>();
+        HashMap<String, HashMap<Character, HashSet<String>>> newTransitions = new HashMap<>();
 
-
-        //combine the alphabets by putting both into a set, preventing repeats, then adding back to a new char list
-        HashSet<Character> newAlphSet = new HashSet<>();
-        for(char letter: nfa1.getAlphabet()) {
-            newAlphSet.add(letter);
-        }
-        for(char letter: nfa2.getAlphabet()) {
-            if(!newAlphSet.contains(letter)) {
-                newAlphSet.add(letter);
-            }
-        }
-        char[] newAlphList = new char[newAlphSet.size()];
-        int i=0;
-        for(Character letter: newAlphSet) {
-            newAlphList[i] = letter;
-            i++;
-        }
-
-
-
-        //update transitions from original dfas to new transitions
-        HashMap<String, HashMap<Character, HashSet<String>>> newTransitions = new HashMap<>(); //state -> (character -> states)
-        //updater contains the original state as a key, and a new unique state name as a value
+        // Helper to create unique state names and keep track of original states to new states mapping
         HashMap<String, String> stateUpdater1 = new HashMap<>();
         HashMap<String, String> stateUpdater2 = new HashMap<>();
 
-        //renames original states to be unique
-        for(String state: nfa1.getStates()) {
-            stateUpdater1.put(state, state+stateCounter);
-            newStates.add(stateUpdater1.get(state));
-            stateCounter++;
-        }
+        // Update state names to be unique and prepare transition mappings
+        updateStateNames(nfa1, newStates, stateUpdater1, newTransitions);
+        updateStateNames(nfa2, newStates, stateUpdater2, newTransitions);
 
-        //renames internal states to match the way they were renamed above, then puts the whole complete transition into newTransitions
-        for(String state: nfa1.getStates()) {
-            HashMap<Character, HashSet<String>> charToInternalStates = new HashMap<>();
-            for(char letter: newAlphList) {
-                HashSet<String> internalStates = nfa2.getTransitions().getOrDefault(state, new HashMap<>()).getOrDefault(letter, new HashSet<>());
-                internalStates = new HashSet<>();
-                for(String internalState: internalStates) {
-                    internalStates.add(stateUpdater1.get(internalState));
-                    internalStates.remove(internalState);
-                }
-                charToInternalStates.put(letter, internalStates);
-            }
-            newTransitions.put(stateUpdater1.get(state),charToInternalStates);
-        }
+        // Merge alphabets from both NFAs
+        char[] mergedAlphabet = mergeAlphabets(nfa1.getAlphabet(), nfa2.getAlphabet());
 
+        //combine transitions
+        HashMap<String, HashMap<Character, HashSet<String>>> combinedTransitions = combineTransitions(nfa1, nfa2, stateUpdater1, stateUpdater2);
 
-        //repeats everything above with nfa2
-        for(String state: nfa2.getStates()) {
-            stateUpdater2.put(state, state+stateCounter);
-            newStates.add(stateUpdater2.get(state));
-            stateCounter++;
-        }
-
-        for(String state: nfa2.getStates()) {
-            HashMap<Character, HashSet<String>> charToInternalStates = new HashMap<>();
-            for(char letter: newAlphList) {
-                HashSet<String> internalStates = nfa2.getTransitions().getOrDefault(state, new HashMap<>()).getOrDefault(letter, new HashSet<>());
-                internalStates = new HashSet<>();
-                for(String internalState: internalStates) {
-                    internalStates.add(stateUpdater2.get(internalState));
-                    internalStates.remove(internalState);
-                }
-                charToInternalStates.put(letter, internalStates);
-            }
-            newTransitions.put(stateUpdater2.get(state),charToInternalStates);
-        }
-
-
-
-        //add new start state and transitions
+        
+        // Create new start state and transitions from it using epsilon transitions to original start states
         String startState = "sUnion";
         newStates.add(startState);
+        HashMap<Character, HashSet<String>> startTransitions = new HashMap<>();
+        HashSet<String> transitionTo = new HashSet<String>();
+        transitionTo.add(stateUpdater1.get(nfa1.getStartState()));
+        transitionTo.add(stateUpdater2.get(nfa2.getStartState()));
+        startTransitions.put('e', transitionTo);
+        combinedTransitions.put(startState, startTransitions);
 
-        //add transitions with e from S_Union to both nfa start states
-        HashMap<Character, HashSet<String>> transition = new HashMap<>();
-        transition.put('e', new HashSet<>(Arrays.asList(stateUpdater1.get(nfa1.getStartState()),stateUpdater2.get(nfa2.getStartState()))));
-        newTransitions.put(startState, transition);
+        // Combine accept states from both NFAs
+        String[] newAcceptStates = combineAcceptStates(nfa1, nfa2, stateUpdater1, stateUpdater2);
 
-        //make newStates a list
-
-        String[] newStatesList = new String[newStates.size()];
-        i = 0;
-        for(String state: newStates) {
-            newStatesList[i] = state;
-            i++;
+        // Create the new NFA
+        Object[] newStatesArrayO = newStates.toArray();
+        String[] newStatesArray = new String[newStatesArrayO.length];
+        int i = 0;
+        for(Object o: newStatesArrayO) {
+        	newStatesArray[i] = o.toString();
+        	i++;
         }
-
-        //combine accept states, just by adding both to a new list
-        String[] newAcceptStates = new String[nfa1.getAcceptStates().length+nfa2.getAcceptStates().length];
-        i=0;
-        for(String state: nfa1.getAcceptStates()) {
-            newAcceptStates[i] = stateUpdater1.get(state);
-            i++;
-        }
-        for(String state: nfa2.getAcceptStates()) {
-            newAcceptStates[i] = stateUpdater2.get(state);
-            i++;
-        }
-
-        //put it all together
-        NFA nfaU = new NFA(newStatesList, newAlphList, newTransitions, startState, newAcceptStates);
+     // Use combinedTransitions to create a new NFA or update an existing one
+        NFA nfaU = new NFA(newStatesArray, mergedAlphabet, combinedTransitions, startState, newAcceptStates);
+      
         return nfaU;
     }
+
+
+    // Implement helper methods
+    private HashMap<String, HashMap<Character, HashSet<String>>> combineTransitions(
+    	    NFA nfa1, NFA nfa2,
+    	    HashMap<String, String> stateUpdater1,
+    	    HashMap<String, String> stateUpdater2) {
+
+    	    // Create a new transitions map to return
+    	    HashMap<String, HashMap<Character, HashSet<String>>> newTransitions = new HashMap<>();
+
+    	    // Combine transitions from nfa1
+    	    for (Map.Entry<String, HashMap<Character, HashSet<String>>> entry : nfa1.getTransitions().entrySet()) {
+    	        String originalState = entry.getKey();
+    	        HashMap<Character, HashSet<String>> transitions = entry.getValue();
+
+    	        // Get the new state name for the original state from nfa1
+    	        String newState = stateUpdater1.get(originalState);
+    	        HashMap<Character, HashSet<String>> newTransStateMap = newTransitions.getOrDefault(newState, new HashMap<>());
+
+    	        for (Map.Entry<Character, HashSet<String>> trans : transitions.entrySet()) {
+    	            Character symbol = trans.getKey();
+    	            HashSet<String> newTargetStates = new HashSet<>();
+
+    	            // Update each target state to its new name
+    	            for (String targetState : trans.getValue()) {
+    	                newTargetStates.add(stateUpdater1.get(targetState));
+    	            }
+
+    	            // Merge transitions for the same character
+    	            if (newTransStateMap.containsKey(symbol)) {
+    	                newTransStateMap.get(symbol).addAll(newTargetStates);
+    	            } else {
+    	                newTransStateMap.put(symbol, newTargetStates);
+    	            }
+    	        }
+
+    	        // Update the new transitions map
+    	        newTransitions.put(newState, newTransStateMap);
+    	    }
+
+    	    // Repeat the same for nfa2
+    	    for (Map.Entry<String, HashMap<Character, HashSet<String>>> entry : nfa2.getTransitions().entrySet()) {
+    	        String originalState = entry.getKey();
+    	        HashMap<Character, HashSet<String>> transitions = entry.getValue();
+
+    	        // Get the new state name for the original state from nfa2
+    	        String newState = stateUpdater2.get(originalState);
+    	        HashMap<Character, HashSet<String>> newTransStateMap = newTransitions.getOrDefault(newState, new HashMap<>());
+
+    	        for (Map.Entry<Character, HashSet<String>> trans : transitions.entrySet()) {
+    	            Character symbol = trans.getKey();
+    	            HashSet<String> newTargetStates = new HashSet<>();
+
+    	            for (String targetState : trans.getValue()) {
+    	                newTargetStates.add(stateUpdater2.get(targetState));
+    	            }
+
+    	            if (newTransStateMap.containsKey(symbol)) {
+    	                newTransStateMap.get(symbol).addAll(newTargetStates);
+    	            } else {
+    	                newTransStateMap.put(symbol, newTargetStates);
+    	            }
+    	        }
+
+    	        // Update the new transitions map
+    	        newTransitions.put(newState, newTransStateMap);
+    	    }
+
+    	    return newTransitions;
+    	}
+
+    
+    
+    private char[] mergeAlphabets(char[] alph1, char[] alph2) {
+        HashSet<Character> alphabetSet = new HashSet<>();
+        for (char c : alph1) {
+            alphabetSet.add(c);
+        }
+        for (char c : alph2) {
+            alphabetSet.add(c);
+        }
+
+        char[] mergedAlphabet = new char[alphabetSet.size()];
+        int i = 0;
+        for (Character c : alphabetSet) {
+            mergedAlphabet[i++] = c;
+        }
+        return mergedAlphabet;
+    }
+
+    
+    private void updateStateNames(NFA nfa, HashSet<String> newStates, HashMap<String, String> stateUpdater, HashMap<String, HashMap<Character, HashSet<String>>> newTransitions) {
+        int stateCounter = 0;  // This assumes stateCounter is declared at a broader scope if needed elsewhere
+
+        // Iterate over each state in the NFA and create a unique new state name
+        for (String state : nfa.getStates()) {
+            String newState = state + "_" + stateCounter++;
+            stateUpdater.put(state, newState);
+            newStates.add(newState);
+
+            // Safely fetch transitions for the state and handle potential null
+            HashMap<Character, HashSet<String>> originalTransitions = nfa.getTransitions().get(state);
+            if (originalTransitions == null) {
+                originalTransitions = new HashMap<>();  // Use an empty map if there are no transitions defined
+            }
+
+            // Update transitions to reflect new state names
+            HashMap<Character, HashSet<String>> updatedTransitions = new HashMap<>();
+            for (Map.Entry<Character, HashSet<String>> entry : originalTransitions.entrySet()) {
+                char transitionChar = entry.getKey();
+                HashSet<String> targetStates = entry.getValue();
+                HashSet<String> updatedTargetStates = new HashSet<>();
+
+                for (String targetState : targetStates) {
+                    updatedTargetStates.add(stateUpdater.get(targetState));  // Update target state to its new name
+                }
+
+                updatedTransitions.put(transitionChar, updatedTargetStates);  // Map the transition character to the updated set of target states
+            }
+
+            newTransitions.put(newState, updatedTransitions);  // Map the new state to its updated transitions
+        }
+    }
+
+    
+    private String[] combineAcceptStates(NFA nfa1, NFA nfa2, HashMap<String, String> stateUpdater1, HashMap<String, String> stateUpdater2) {
+        String[] acceptStates1 = nfa1.getAcceptStates();
+        String[] acceptStates2 = nfa2.getAcceptStates();
+        String[] newAcceptStates = new String[acceptStates1.length + acceptStates2.length];
+
+        int i = 0;
+        for (String state : acceptStates1) {
+            newAcceptStates[i++] = stateUpdater1.get(state); // Update state names in the list of accept states for NFA1
+        }
+        for (String state : acceptStates2) {
+            newAcceptStates[i++] = stateUpdater2.get(state); // Update state names in the list of accept states for NFA2
+        }
+
+        return newAcceptStates;
+    }
+
+    
+    
+
 
     // TODO: Complete this method so that it returns the nfa resulting from concatenating the two input nfas.
     private NFA concatenate(NFA nfa1, NFA nfa2) {
@@ -337,6 +418,8 @@ public class RegularExpression {
         NFA nfaSingleChar = new NFA(states, alphabet, transitions, states[0],acceptStates);
         return nfaSingleChar;
     }
+    
+    
 
     // You are not allowed to change this method's header at all.
     public boolean test(String string) {
